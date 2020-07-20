32347622d5161320e49a2bc91ded54e053d9f1de
load28 <tjalsdud89@naver.com>
2020-07-20 21:54:01 +0900

    add test case: fromNullabel option function

--- src/fp-ts/option.ts
+++ src/fp-ts/option.ts
@@ -1,4 +1,4 @@
-import { Option, some, none } from 'fp-ts/lib/Option';
+import { Option, some, none, fromNullable } from 'fp-ts/lib/Option';
 
 export function findIndex<T>(
   as: T[],
@@ -6,4 +6,9 @@ export function findIndex<T>(
 ): Option<number> {
   const index = as.findIndex(predicate);
   return index === -1 ? none : some(index);
+}
+
+export function find<T>(as: T[], predicate: (a: T) => boolean): Option<T> {
+  const finedItem: T = as.find(predicate) as T;
+  return fromNullable<T>(finedItem);
 }
--- test/option.spec.ts
+++ test/option.spec.ts
@@ -1,10 +1,19 @@
-import { findIndex } from '../src/fp-ts';
-import { isSome, some } from 'fp-ts/lib/Option';
+import { find, findIndex } from '../src/fp-ts';
+import { isSome, none, some } from 'fp-ts/lib/Option';
 
 describe('option', () => {
-  test('basic option', () => {
+  test('none/some option', () => {
     const result = findIndex<number>([1, 2, 3, 4], (a: number) => a === 1);
+
     expect(isSome(result)).toStrictEqual(true);
     expect(result).toStrictEqual(some(0));
+  });
+
+  test('fromNullable option', () => {
+    const result_1 = find<number>([1, 2, 3, 4], (a: number) => a === 4);
+    const result_2 = find<number>([1, 2, 3, 4], (a: number) => a === 5);
+
+    expect(isSome(result_1)).toStrictEqual(true);
+    expect(result_2).toStrictEqual(none);
   });
 });
